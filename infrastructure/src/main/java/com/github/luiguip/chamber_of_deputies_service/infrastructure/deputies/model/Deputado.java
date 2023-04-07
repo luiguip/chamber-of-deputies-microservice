999eb287c71b9441fbd738dc83a5fb9ed7024cb4
luiguip <luisguilhermepdea@gmail.com>
2023-04-07 10:46:03 -0300

    Added wsdl entities

---
+++ infrastructure/src/main/java/com/github/luiguip/chamber_of_deputies_service/infrastructure/deputies/model/Deputado.java
@@ -0,0 +1,22 @@
+package com.github.luiguip.chamber_of_deputies_service.infrastructure.deputies.model;
+
+public class Deputado {
+  private Integer ideCadastro;
+  private Integer codOrcamento;
+  private String condicao;
+  private Integer matricula;
+  private Long idParlamentar;
+  private String nome;
+  private String nomeParlamentar;
+  private String urlFoto;
+  private String sexo;
+  private String uf;
+  private String partido;
+  private Integer gabinete;
+  private Integer anexo;
+  private String fone;
+  private String email;
+
+
+
+}
